68ca15b42daf46c0ee5ecc0e79bb6d476e233c12
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-15 10:59:12 +0000

    header structure for both mobile & desktop

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,20 @@
 <header class="site-header">
+
+  <ul class="mobile-top">
+    <li><a href="tel:0800123456">0800 123 456</a></li>
+    <li><a href="#">Become A Guest</a></li>
+  </ul>
+
   <div class="header-inner">
 
-    Header Inner
+    <div class="site-logo">
+      <a href="index.php">Logo</a>
+    </div>
+
+    <div class="mobile-actions">
+      <a href="#" class="menu-toggle">Menu</a>
+      <a href="#" class="icon-yellow">Yellow Icon</a>
+    </div>
 
     <div class="header-top">
       <a href="tel:0800123456">0800 123 456</a>
@@ -13,34 +26,43 @@
         </form>
         <a href="#" class="search-toggle">Search</a>
       </div>
+  
+      <nav class="primary">
+        <ul>
+          <li class="has-children"><a href="#">Podcasts</a>
+            <div class="submenu">
+              
+              <div class="col">
+                Column One
+              </div>
+
+              <div class="col">
+                Column Two
+              </div>
+
+              <div class="col-slim">
+                Column Three
+              </div>
+
+            </div>
+          </li>
+          <li><a href="#">Our Partners</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Training &amp; CPD</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+
     </div>
 
-    <nav class="primary">
-      <ul>
-        <li class="has-children"><a href="#">Podcasts</a>
-          <div class="submenu">
-            
-            <div class="col">
-              Column One
-            </div>
-
-            <div class="col">
-              Column Two
-            </div>
-
-            <div class="col-slim">
-              Column Three
-            </div>
-
-          </div>
-        </li>
-        <li><a href="#">Our Partners</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Training &amp; CPD</a></li>
-        <li><a href="#">Events</a></li>
-        <li><a href="#">About</a></li>
-      </ul>
-    </nav>
-
   </div>
+
+  <div class="search-container">
+    <form>
+      <input type="text" placeholder="Search Our Website">
+      <button type="submit"><img src="images/search.png"></button>
+    </form>
+  </div>
+
 </header>
