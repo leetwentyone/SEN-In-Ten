db037a44a59becb4fa19903fe4cf002d9dd9b0d5
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-15 08:52:30 +0000

    Initial template files

---
+++ includes/header.php
@@ -0,0 +1,46 @@
+<header class="site-header">
+  <div class="header-inner">
+
+    Header Inner
+
+    <div class="header-top">
+      <a href="tel:0800123456">0800 123 456</a>
+      <a href="#" class="icon-white">White Icon</a>
+      <a href="#" class="icon-yellow">Yellow Icon</a>
+      <div class="search-container">
+        <form>
+          <input type="text" placeholder="Search Our Website">
+        </form>
+        <a href="#" class="search-toggle">Search</a>
+      </div>
+    </div>
+
+    <nav class="primary">
+      <ul>
+        <li class="has-children"><a href="#">Podcasts</a>
+          <div class="submenu">
+            
+            <div class="col">
+              Column One
+            </div>
+
+            <div class="col">
+              Column Two
+            </div>
+
+            <div class="col-slim">
+              Column Three
+            </div>
+
+          </div>
+        </li>
+        <li><a href="#">Our Partners</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Training &amp; CPD</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="home">
+
+  <?php include('includes/header.php'); ?>
+
+  <main class="site-main">
+
+    Main Page
+    
+  </main>
+
+  <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
