73a6fb92ac0d9af352bb3005a0cf627b78b6d98d
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-20 16:46:19 +0000

    podcast page template

---
+++ podcast.php
@@ -0,0 +1,48 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="podcasts-page">
+
+  <?php include('includes/header.php'); ?>
+  <?php include('includes/breadcrumb.php'); ?>
+
+  <main class="site-main">
+
+    <div class="textblock">
+        <h1>Episode 1: OrCam Learn</h1>
+    </div>
+
+    <div class="podcast-summary">
+    
+        <div class="image-col">
+            <img src="images/square-placeholder.jpg">
+        </div>
+
+        <div class="episode-details">
+            <h3>Episode Summary</h3>
+            <p>Praesent euismod auctor lectus, pulvinar aliquam nulla auctor non. Pellentesque tristique pellentesque sem, a condimentum magna viverra in. 
+                Vestibulum ut lacinia purus. Praesent molestie rutrum fringilla. </p>
+            <a href="#">View Podcast Notes</a>
+        </div>
+
+    </div><!-- end latest podcast -->
+
+  </main>
+
+  <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
