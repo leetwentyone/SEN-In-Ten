635a184d1ad30e34b7400810b981d181e721a5bc
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-20 11:19:22 +0000

    Added slider functionality for partners & episodes

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,77 @@
 jQuery(function($) {
-    jQuery('.search-toggle').click(function(e) {
-      e.preventDefault();
-      jQuery(".search-container").toggleClass("search-active");
-    })
+  jQuery('.search-toggle').click(function(e) {
+    e.preventDefault();
+    jQuery(".search-container").toggleClass("search-active");
+  })
 });
 
 jQuery(function($) {
-    jQuery('.close-search').click(function(e) {
-      e.preventDefault();
-      jQuery(".search-container").removeClass("search-active");
-    })
+  jQuery('.close-search').click(function(e) {
+    e.preventDefault();
+    jQuery(".search-container").removeClass("search-active");
+  })
+});
+
+/* Episode List */
+jQuery(document).ready(function(){
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    jQuery('.episode-list ul').slick({
+      autoplay: true,
+      autoplaySpeed: 6000,
+      slidesToShow: 3,
+      dots: false,
+      arrows: false,
+      responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+    });
+  }
+});
+
+/* Our Partners */
+jQuery(document).ready(function(){
+  jQuery('.partners ul').slick({
+    autoplay: true,
+    autoplaySpeed: 6000,
+    slidesToShow: 6,
+    dots: false,
+    arrows: true,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
 });
