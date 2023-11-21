7b25ee6c24e4edd1ce75196f6407adb11cb5fb0a
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-21 11:43:10 +0000

    new subscribe include file

---
+++ includes/subscribe.php
@@ -0,0 +1,21 @@
+<div class="subscribe">
+    <div class="c-container">
+    <div class="form-col">
+        <h5>Subscribe to our mailing list</h5>
+        <form>
+        <input type="text" placeholder="Name">
+        <input type="email" placeholder="Email Address">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur dapibus sollicitudin. Sed at nisi finibus, laoreet sem sed, consequat mi.</p>
+        <button type="submit">Submit</button>
+        </form>
+    </div>
+    <div class="quote-col">
+        <div class="quote-box">
+        <blockquote>
+            "This is a quote for this column"
+        </blockquote>
+        <cite>Name of Person</cite>
+        </div>
+    </div>
+    </div>
+</div><!-- End Subscribe -->
