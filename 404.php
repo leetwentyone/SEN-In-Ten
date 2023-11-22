1a1936f2033de96f50464156433b1cae8c32797c
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-22 11:49:34 +0000

    404 template

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="error-page">
+
+    <?php include('includes/header.php'); ?>
+
+    <main class="site-main">
+
+       <div class="error-text">
+           <h1>We need a re-tune!</h1>
+           <p>Cras vitae rhoncus tortor, nec congue neque.</p>
+           <a href="contanct.php">Contact Us</a>
+       </div> 
+
+    </main>
+  
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
