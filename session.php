871510d78b05d1e9eb02b9e9564dba5e4d670e56
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-22 09:26:56 +0000

    session template

---
+++ session.php
@@ -0,0 +1,194 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="single-session">
+
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/breadcrumb.php'); ?>
+
+    <main class="site-main">
+
+        <div class="textblock">
+            <h1>Training Session</h1>
+        </div>
+
+        <div class="session-summary">
+        
+            <date>8th<br>Dec</date>
+            <div class="image-col">
+                <img src="images/square-placeholder.jpg">
+            </div>
+
+            <div class="session-overview">
+                <p>Praesent euismod auctor lectus, pulvinar aliquam nulla auctor non. Pellentesque tristique pellentesque sem, a condimentum magna viverra in. 
+                    Vestibulum ut lacinia purus. Praesent molestie rutrum fringilla.
+                </p>
+                <ul class="session-details">
+                    <li class="time">8pm - 9pm</li>
+                    <li class="date">Monday 6th February 2023</li>
+                    <li class="location">Arena</li>
+                </ul>
+                <ul class="session-actions">
+                    <a href="#" target="_blank">Book Via Eventbrite</a>
+                    <a href="#" target="_blank">Book Via Zoom</a>
+                </ul>
+                <div class="share">
+                    <div class="share-title">Share Event</div>
+                    <ul>
+                        <li><a href="#">F</a></li>
+                        <li><a href="#">X</a></li>
+                        <li><a href="#">In</a></li>
+                        <li><a href="#">IG</a></li>
+                        <li><a href="#">YT</a></li>
+                        <li><a href="#">TT</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div><!-- end latest podcast -->
+
+        <div class="banner-callout">
+            -- Parent Pay Banner to be placed here --
+        </div><!-- end banner callout -->
+
+        <div class="text-image is-reversed">
+            <div class="c-container">
+
+                <div class="image-col">
+                <img src="images/square-placeholder.jpg">
+                </div>
+
+                <div class="text-col">
+                <h5>More Information</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur dapibus sollicitudin. Sed at nisi finibus, laoreet sem sed, consequat mi.</p>
+                <p>Etiam mattis quis nulla eget lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim leo, mattis id efficitur sit amet, lobortis tincidunt urna. 
+                    Ut dapibus est leo, quis ornare orci egestas eget. Fusce ut luctus mi, nec cursus massa. Etiam nec imperdiet erat.</p>
+                <a href="#">Make An Enquiry</a>
+                </div>
+
+            </div>
+        </div><!-- end Text Image Block -->
+
+        <div class="faqs accordion">
+            
+            <h4>FAQs</h4>
+
+            <div class="accordion-container">
+
+                <div class="accordion-item">
+                    <div class="accordion-heading">This is the heading for this accordion item</div>
+                    <div class="accordion-content" style="display:none;">
+                        <p>Fusce eu luctus enim, a facilisis purus. Praesent neque mauris, cursus sed imperdiet a, tempor eget arcu. 
+                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent maximus pellentesque velit, 
+                            ut dapibus ipsum sagittis id. Vivamus ultricies nisl vitae tortor vehicula, sit amet tempor neque eleifend. Suspendisse potenti. 
+                            Proin euismod non lacus a elementum. Cras nisi dolor, pretium a metus id, suscipit maximus lorem.
+                        </p>
+
+                        <p>Suspendisse rutrum mollis metus, sit amet pulvinar tortor suscipit in. Curabitur sagittis consectetur felis nec consectetur. 
+                            Sed nec porta ex. Praesent lobortis ex ut odio luctus, ac egestas est efficitur. Fusce non placerat nisi, eget ultrices nisl. 
+                            Donec sed vehicula lectus. Aliquam scelerisque enim vitae facilisis condimentum. Cras blandit pellentesque iaculis. 
+                            Ut turpis eros, auctor a consectetur sed, semper ac dui. Donec molestie fermentum tellus, eget ullamcorper sapien dapibus eget.
+                        </p>        
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <div class="accordion-heading">This is the heading for this accordion item</div>
+                    <div class="accordion-content" style="display:none;">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in auctor erat, eu mattis sem. Donec bibendum ultricies tortor non tincidunt.</p>
+                    <p>In urna sapien, sagittis vitae semper a, dictum non urna. Nam vehicula sit amet diam eu sagittis. Nunc tristique mauris vitae mauris tempor, non venenatis leo gravida. Vestibulum auctor auctor est, sit amet vestibulum dui aliquet vitae. <strong>Proin a dignissim elit</strong>. Proin accumsan semper porta.</p>
+                    <p>Nunc sed sodales mi. Donec fermentum fermentum lorem, sed fermentum nulla blandit sit amet. Ut pulvinar lectus nec metus rhoncus, at maximus purus laoreet. Aliquam justo risus, pretium imperdiet dignissim vel, accumsan et dolor. <a href="#">Nullam sed nibh dolor</a>. Integer malesuada nisi nulla, id interdum tortor facilisis ac. Sed id molestie leo. Sed viverra sit amet lorem ut finibus. Sed cursus arcu eget blandit condimentum.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div><!-- End FAQs -->
+
+        <div class="enquiry" id="enquire">
+            <div class="c-container">
+
+                <h3>Make An Enquiry</h3>
+
+                <form>
+                    <fieldset>
+                        <legend>Your Details</legend>
+                        <div class="form-row">
+                            <div class="field-50">
+                                <input type="text" placeholder="First Name*">
+                            </div>
+                            <div class="field-50">
+                                <input type="text" placeholder="Last Name*">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="field-100">
+                                <input type="email" placeholder="Email Address*">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="field-100">
+                                <textarea placeholder="Your Enquiry*"></textarea>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Additional Information</legend>
+                        <div class="form-row">
+                            <div class="field-50">
+                                <input type="text" placeholder="Telephone Number">
+                            </div>
+                            <div class="field-50">
+                                <input type="text" placeholder="Organisation Name">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="field-100">
+                                <input type="email" placeholder="Website">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="field-100">
+                                <input type="text" placeholder="How did you hear about us?*">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="field-100">
+                                <input type="radio">I agree to the data <a href="#">terms &amp; conditions</a>
+                            </div>
+                            <p><a href="#">Read our Privacy Policy</a></p>
+                        </div>
+                        <div class="form-row centered">
+                            -- ReCaptcha --
+                        </div>
+                        <div class="form-row centered">
+                            <button type="submit">Submit</button>
+                        </div>
+                    </fieldset>
+                </form>
+
+            </div>
+        </div><!-- end enquiry form -->
+
+        <?php include('includes/subscribe.php'); ?>
+
+    </main>
+  
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
