c2c2144b07da8f5b8cf94c99a36494dadae5495d
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-22 10:28:38 +0000

    become a sponsor template

---
+++ become-sponsor.php
@@ -0,0 +1,60 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="become-sponsor-page">
+
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/breadcrumb.php'); ?>
+
+    <main class="site-main">
+
+        <div class="textblock">
+            <h1>Become A Sponsor</h1>
+            <p>Vestibulum cursus pellentesque felis ac sodales. Phasellus rhoncus diam at arcu tincidunt, in vulputate tellus accumsan. 
+            ullamcorper congue massa. Proin pharetra ultricies ullamcorper congue massa. Proin pharetra ultricies</p>
+            <p>Nullam condimentum varius elit, vel ultricies ex iaculis in. Proin euismod non lacus a elementum. Cras nisi dolor, pretium a metus</p>
+        </div>
+
+        <div class="whats-included">
+            <div class="c-container">
+
+                <div class="image-col">
+                    <img src="images/square-placeholder.jpg">
+                </div>
+
+                <div class="text-col">
+                    <h5>What's Included?</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur dapibus sollicitudin. Sed at nisi finibus, laoreet sem sed, consequat mi.</p>
+                    <ul>
+                        <li>Suspendisse rutrum mollis metus, sit amet pulvinar tortor suscipit in. Curabitur sagittis consectetur felis nec consectetur.</li>
+                        <li>Ut turpis eros, auctor a consectetur sed, semper ac dui. Donec molestie</li>
+                        <li>Vestibulum cursus pellentesque felis ac sodales. Phasellus rhoncus diam at arcu tincidunt, in vulputate tellus accumsan.</li>
+                        <li>Cras id aliquam lectus, quis sagittis nisi. Cras vitae rhoncus tortor, nec congue neque.</li>
+                    </ul>
+                    <div class="btn-container">
+                        <a href="#">Make An Enquiry</a>
+                    </div>
+                </div>
+
+            </div>
+        </div><!-- end Text Image Block -->
+
+    </main>
+  
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
