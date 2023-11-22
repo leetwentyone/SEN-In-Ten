7db2bec4b24e9fb986ce1f11cd5c4710591296f5
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-22 09:26:47 +0000

    events template

---
+++ events.php
@@ -0,0 +1,131 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="events-page">
+
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/breadcrumb.php'); ?>
+
+    <main class="site-main">
+
+        <div class="textblock">
+            <h1>Events</h1>
+            <p>Praesent euismod auctor lectus, pulvinar aliquam nulla auctor non. Pellentesque tristique pellentesque sem, a condimentum magna viverra in.</p>
+        </div>
+
+        <div class="featured-event">
+        
+            <date>8th<br>Dec</date>
+            <div class="image-col">
+                <img src="images/square-placeholder.jpg">
+            </div>
+
+            <div class="event-overview">
+                <p>Featured Event</p>
+                <h3>Event Name</h3>
+                <p>Praesent euismod auctor lectus, pulvinar aliquam nulla auctor non. Pellentesque tristique pellentesque sem, a condimentum magna viverra in. 
+                    Vestibulum ut lacinia purus. Praesent molestie rutrum fringilla.
+                </p>
+                <ul class="event-details">
+                    <li class="time">8pm - 9pm</li>
+                    <li class="date">Monday 6th February 2023</li>
+                    <li class="location">Arena</li>
+                </ul>
+                <ul class="event-actions">
+                    <a href="#" target="_blank">Book Via Eventbrite</a>
+                    <a href="#" target="_blank">Book Via Zoom</a>
+                </ul>
+            </div>
+
+        </div><!-- end featured event -->
+
+        <div class="event-listing">
+            <div class="c-container">
+
+                <h4>Upcoming Events</h4>
+
+                <ul>
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img src="images/square-placeholder.jpg">
+                        
+                        <div class="event-content">
+                            <h5><a href="event.php">Event Name</a></h5>
+                            <a href="event.php">Read More</a>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div><!-- end episode list -->
+
+        <div class="banner-callout">
+            -- Parent Pay Banner to be placed here --
+        </div><!-- end banner callout -->
+
+        <?php include('includes/subscribe.php'); ?>
+
+    </main>
+  
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
