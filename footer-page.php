7b94d61f3cc00cebbb08bc449bf81c7e21fa3e04
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2023-11-22 11:50:35 +0000

    added google font link

---
+++ footer-page.php
@@ -0,0 +1,74 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SEN in Ten</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css?v=1.0">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body class="misc-page">
+
+    <?php include('includes/header.php'); ?>
+
+    <main class="site-main">
+
+        <div class="textblock">
+            <h1>Privacy Policy</h1>
+            <p>Vestibulum cursus pellentesque felis ac sodales. Phasellus rhoncus diam at arcu tincidunt, in vulputate tellus accumsan. 
+            Nullam condimentum varius elit, vel ultricies ex iaculis in.
+            </p>
+        </div>
+
+        <div class="accordion" style="padding-top: 0;">
+
+            <div class="accordion-container">
+
+                <div class="accordion-item">
+                    <div class="accordion-heading">This is the heading for this accordion item</div>
+                    <div class="accordion-content" style="display:none;">
+                        <p>Fusce eu luctus enim, a facilisis purus. Praesent neque mauris, cursus sed imperdiet a, tempor eget arcu. 
+                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent maximus pellentesque velit, 
+                            ut dapibus ipsum sagittis id. Vivamus ultricies nisl vitae tortor vehicula, sit amet tempor neque eleifend. Suspendisse potenti. 
+                            Proin euismod non lacus a elementum. Cras nisi dolor, pretium a metus id, suscipit maximus lorem.
+                        </p>
+
+                        <p>Suspendisse rutrum mollis metus, sit amet pulvinar tortor suscipit in. Curabitur sagittis consectetur felis nec consectetur. 
+                            Sed nec porta ex. Praesent lobortis ex ut odio luctus, ac egestas est efficitur. Fusce non placerat nisi, eget ultrices nisl. 
+                            Donec sed vehicula lectus. Aliquam scelerisque enim vitae facilisis condimentum. Cras blandit pellentesque iaculis. 
+                            Ut turpis eros, auctor a consectetur sed, semper ac dui. Donec molestie fermentum tellus, eget ullamcorper sapien dapibus eget.
+                        </p>        
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <div class="accordion-heading">This is the heading for this accordion item</div>
+                    <div class="accordion-content" style="display:none;">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in auctor erat, eu mattis sem. Donec bibendum ultricies tortor non tincidunt.</p>
+                    <p>In urna sapien, sagittis vitae semper a, dictum non urna. Nam vehicula sit amet diam eu sagittis. Nunc tristique mauris vitae mauris tempor, non venenatis leo gravida. Vestibulum auctor auctor est, sit amet vestibulum dui aliquet vitae. <strong>Proin a dignissim elit</strong>. Proin accumsan semper porta.</p>
+                    <p>Nunc sed sodales mi. Donec fermentum fermentum lorem, sed fermentum nulla blandit sit amet. Ut pulvinar lectus nec metus rhoncus, at maximus purus laoreet. Aliquam justo risus, pretium imperdiet dignissim vel, accumsan et dolor. <a href="#">Nullam sed nibh dolor</a>. Integer malesuada nisi nulla, id interdum tortor facilisis ac. Sed id molestie leo. Sed viverra sit amet lorem ut finibus. Sed cursus arcu eget blandit condimentum.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div><!-- End FAQs -->
+
+        <?php include('includes/subscribe.php'); ?>
+
+    </main>
+  
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
